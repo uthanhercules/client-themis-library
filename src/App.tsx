d7bae89c6ebdc7cbd6842d63e66f4b4b8d098771
Uthan H <uthan.hercules12@gmail.com>
2022-04-08 07:17:53 -0300

    App.scss changed to global.scss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-import './App.scss';
+import './shared/scss/global.scss';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
