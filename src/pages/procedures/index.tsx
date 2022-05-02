0738b292fc6d7a96ffb56efde29d998f387b5e1a
Uthan H <uthan.hercules12@gmail.com>
2022-05-02 19:06:54 -0300

    Procedures list UI finished

--- src/pages/procedures/index.tsx
+++ src/pages/procedures/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../../shared/components/Header";
 import LoginInput from "../../shared/components/LoginInput";
+import ProcedureListItem from "../../shared/components/ProcedureListItem";
 import "./style.scss";
 
 const index = () => {
@@ -16,6 +17,30 @@ const index = () => {
           action={() => console.log("Pesquisei")}
         />
         <button type="button">+ Novo Processo</button>
+      </article>
+      <article className="procedure-list">
+        <header>
+          <span className="procedure-number">Número</span>
+          <span className="procedure-name">Nome</span>
+          <span className="procedure-customer">Cliente</span>
+          <span className="procedure-edit"></span>
+          <span className="procedure-delete"></span>
+        </header>
+        <ProcedureListItem
+          procedureNumber="12345678901234567890"
+          name="Processo contra o mundo inteirinhozinho"
+          customer="Uthan Hercules"
+        />
+        <ProcedureListItem
+          procedureNumber="12345678901234567890"
+          name="Processo contra o mundo inteirinhozinho"
+          customer="Uthan Hercules"
+        />
+        <ProcedureListItem
+          procedureNumber="12345678901234567890"
+          name="Processo contra o mundo inteirinhozinho"
+          customer="Uthan Hercules"
+        />
       </article>
     </main>
   );
