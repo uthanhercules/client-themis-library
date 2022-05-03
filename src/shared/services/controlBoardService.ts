34f91be38d21e91e0b5612450d8c15a2aaf80798
Uthan H <uthan.hercules12@gmail.com>
2022-05-03 13:43:38 -0300

    Services now have theirs endpoints

--- src/shared/services/controlBoardService.ts
+++ src/shared/services/controlBoardService.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000/";
+const BASE_URL = "http://localhost:8000";
 
 interface INewProcedure {
   customer_id: string;
@@ -20,8 +20,8 @@ interface IAdminData {
   email: string;
 }
 
-async function createNewProcedure(point: string, data: INewProcedure) {
-  const response = await fetch(BASE_URL + point, {
+async function createNewProcedure(data: INewProcedure) {
+  const response = await fetch(BASE_URL + "/procedure/create", {
     method: "POST",
     body: JSON.stringify(data),
     headers: {
@@ -34,8 +34,8 @@ async function createNewProcedure(point: string, data: INewProcedure) {
   return { data: responseData, ok: response.ok };
 }
 
-async function createNewCustomer(point: string, data: INewCustomer) {
-  const response = await fetch(BASE_URL + point, {
+async function createNewCustomer(data: INewCustomer) {
+  const response = await fetch(BASE_URL + "/customer/create", {
     method: "POST",
     body: JSON.stringify(data),
     headers: {
@@ -48,8 +48,8 @@ async function createNewCustomer(point: string, data: INewCustomer) {
   return { data: responseData, ok: response.ok };
 }
 
-async function editAdminData(point: string, data: IAdminData) {
-  const response = await fetch(BASE_URL + point, {
+async function editAdminData(data: IAdminData) {
+  const response = await fetch(BASE_URL + "/admin", {
     method: "PUT",
     body: JSON.stringify(data),
     headers: {
