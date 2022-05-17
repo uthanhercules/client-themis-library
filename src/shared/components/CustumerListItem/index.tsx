d2a3b7998c18c22afef4f3412e7791f2ec2b0738
Duda Furtado <furtadoduda@hotmail.com>
2022-05-17 13:05:00 -0300

    The component are use inside the map to list all the deatils of customers are here

--- src/shared/components/CustumerListItem/index.tsx
+++ src/shared/components/CustumerListItem/index.tsx
@@ -1,26 +1,65 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { toast } from "react-toastify";
+import apiService from "../../services/allCustomerService"
+import PopUpToDeleteCustomer from "../PopUpToDeleteOrNot";
 import deleteIcon from "../../assets/delete-icon.svg";
 import editIcon from "../../assets/edit-icon.svg";
 import "./style.scss";
 
 interface ICustomerListItem {
-    fullName: string;
-    email: string;
-  }
+  key: string;
+  fullName: string;
+  email: string;
+  customerList: object;
+  setCustomerList: object;
+}
+
+interface IDeleteCustomer {
+  key: string;
+  customerList: object;
+  setCustomerList: object;
+}
   
-  const index = ({ fullName, email }: ICustomerListItem) => {
-    return (
-      <section className="customers-item">
-        <NavLink to={`/customers/${fullName}`}>{fullName}</NavLink>
-        <span className="c-email">{email}</span>
-        <button className="c-edit">
-          <img src={editIcon} alt="Edit Button" />
-        </button>
-        <button className="c-delete">
-          <img src={deleteIcon} alt="Delete Button" />
-        </button>
-      </section>
-    );
-  };
+const index = ({ key, fullName, email, customerList, setCustomerList }: ICustomerListItem) => {
+
+  const [popUp, setPopUp] = useState(false);
+
+  async function handleDeleteCustomer ({ key, customerList, setCustomerList }: IDeleteCustomer) {
+    const userToken = localStorage.getItem("userToken");
+    try {
+      const { data, ok } = await apiService.deleteCustomer("userToken");
+
+      const localCustomers = [...customerList];
+      const customerDelete = localCustomers.findIndex((customer) => customer.id === key);
+      localCustomers.splice(customerDelete, 1);
+
+      setCustomerList(localCustomers);
+    }catch(error) {
+      return toast.error();
+    }
+  }
+
+  return (
+    <section className="customers-item">
+      <NavLink to={`/customers/${fullName}`}>{fullName}</NavLink>
+      <span className="c-email">{email}</span>
+      <button className="c-edit">
+        <img src={editIcon} alt="Edit Button" />
+      </button>
+      <button 
+      className="c-delete"
+      onClick={() => popUp ? setPopUp(false) : setPopUp(true)}>
+        <img src={deleteIcon} alt="Delete Button" />
+      </button>
+      { popUp &&
+      <PopUpToDeleteCustomer
+      setPopUp={setPopUp}
+      handleDeleteCustomer={handleDeleteCustomer}
+      />
+      }
+    </section>
+  );
+};
   
   export default index;
