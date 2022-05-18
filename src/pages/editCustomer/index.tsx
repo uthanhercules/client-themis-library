bbe18fbfa7fce91d27be18c7506918be13290374
Uthan H <uthan.hercules12@gmail.com>
2022-05-18 19:10:45 -0300

    Created editCustomer page

---
+++ src/pages/editCustomer/index.tsx
@@ -0,0 +1,64 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import Header from "../../shared/components/Header";
+import Input from "../../shared/components/LoginInput";
+import "./style.scss";
+
+import customerServices from "../../shared/services/customerServices";
+
+const index = () => {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+
+  async function createCustomer(e: any) {
+    e.preventDefault();
+
+    const userToken = localStorage.getItem("userToken");
+    if (!userToken) {
+      toast.error("Você precisa estar logado para fazer isso.");
+      localStorage.removeItem("userToken");
+      return (window.location.href = "/login");
+    }
+
+    if (fullName.trim() === "")
+      return toast.error("Nome Completo é um campo obrigatório");
+    if (email.trim() === "")
+      return toast.error("E-mail é um campo obrigatório");
+
+    const { data, ok } = await customerServices.createNewCustomer(
+      {
+        full_name: fullName,
+        email,
+      },
+      userToken
+    );
+    if (!ok) return toast.error(data);
+
+    return toast.success("Cliente criado com sucesso!");
+  }
+
+  return (
+    <article className="new-procedure-page">
+      <Header />
+      <h1>Edit Cliente</h1>
+      <form className="new-procedure-data">
+        <Input
+          className="g-input"
+          type="text"
+          placeholder="Nome Completo"
+          action={setFullName}
+        />
+        <Input
+          className="g-input"
+          type="email"
+          placeholder="E-mail"
+          action={setEmail}
+        />
+        <button onClick={(e) => createCustomer(e)}>Criar Novo Cliente</button>
+      </form>
+    </article>
+  );
+};
+
+export default index;
